7a936b44e44d130baa0c7e73545f51ea9a7849e1
jefferytcc <jefferytcc91@gmail.com>
2017-03-18 17:34:34 +0800

    add secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318085221) do
+ActiveRecord::Schema.define(version: 20170318091302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
