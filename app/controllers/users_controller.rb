ea8697397bb0c324b53201ff87a7c4290bee85c7
jefferytcc <jefferytcc91@gmail.com>
2017-03-18 18:16:33 +0800

    basic sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/new
@@ -26,14 +27,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
+
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+     
+        render :new 
+
+
     end
   end
 
